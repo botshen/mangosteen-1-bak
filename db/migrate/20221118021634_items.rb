655a15690febbb4bb3987255807050f5a582bd1c
shenxin <363088847@qq.com>
2022-11-18 12:36:32 +0800

    测试 validation_controller

--- db/migrate/20221118021634_items.rb
+++
@@ -1,5 +0,0 @@
-class Items < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :items
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_18_021634) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "items", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "amount"
+    t.text "note"
+    t.bigint "tags_id", array: true
+    t.datetime "happen_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "name"
