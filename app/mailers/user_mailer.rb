9a14fc796c84c9972e619b21b177a0531691ec1d
shenxin <363088847@qq.com>
2022-12-18 22:39:49 +0800

    测试邮件被成功发送

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
-  def welcome_email(code)
-    @code = code
-    mail(to: "botshen@foxmail.com", subject: "Welcome to My Awesome Site")
+  def welcome_email(email)
+    validation_code = ValidationCode.find_by_email(email)
+    @code = validation_code.code
+    mail(to: email, subject: '山竹记账验证码')
   end
 end
