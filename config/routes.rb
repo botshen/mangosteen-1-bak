98a23fcc0dd3f8ab529bf05e61c54f7bd765ba98
shenxin <363088847@qq.com>
2023-03-01 14:14:03 +0800

    增加 /items/balance 接口，获取一段时间范围内的余额

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 require 'action_dispatch/middleware/static'
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/", to: "home#index"
+  get '/', to: 'home#index'
 
   namespace :api do
     namespace :v1 do
       resources :validation_codes, only: [:create]
       resource :session, only: [:create, :destroy]
       resource :me, only: [:show]
-      resources :items do 
-        collection do 
+      resources :items do
+        collection do
+          get :balance
           get :summary
         end
       end
       resources :tags
     end
   end
+
 end
